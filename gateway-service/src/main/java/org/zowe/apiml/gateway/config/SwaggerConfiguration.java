984fc8ccb4fc24b7813e8505f7708fd9f665a45a
Andrea Tabone <andrea.tabone@broadcom.com>
2022-06-16 11:05:42 +0200

    Add missing file
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

---
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/config/SwaggerConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.gateway.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Value("${apiml.service.title}")
+    private String apiTitle;
+
+    @Value("${eureka.instance.metadata-map.apiml.apiInfo[0].version}")
+    private String apiVersion;
+
+    @Value("${apiml.service.description}")
+    private String apiDescription;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info()
+            .title(apiTitle)
+            .description(apiDescription)
+            .version(apiVersion)
+        ).components(new Components()
+                .addSecuritySchemes("Basic authorization", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic"))
+                .addSecuritySchemes("CookieAuth", new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER).name("apimlAuthenticationToken"))
+                .addSecuritySchemes("Bearer", new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("apimlAuthenticationToken"))
+            );
+    }
+
+    @Bean
+    public GroupedOpenApi storeOpenApi() {
+        String paths[] = {"**/auth/**"};
+        return GroupedOpenApi.builder().group("auth").pathsToMatch(paths)
+            .build();
+    }
+}
