a4d949f8e10243474a67999d4eab9f043c30879e
Petr Weinfurt <petr.weinfurt@broadcom.com>
2022-12-05 12:58:08 +0100

    fix "error:0308010C:digital envelope routines:inner_evp_generic_fetch:unsupported" during extraction of private key
    Signed-off-by: Petr Weinfurt <petr.weinfurt@broadcom.com>

--- docker/redis/run-redis.sh
+++ docker/redis/run-redis.sh
@@ -173,7 +173,7 @@ genKeyPairCert apimtst
 genKeyPairCert user
 genKeyPairCert unknownuser
 log "Extracting private key"
-openssl pkcs12 -in "${KEYSTORE}" -nocerts -out "${KEYSTORE_DIR}/all-services.keystore.key" -passin pass:password -passout pass:password
+openssl pkcs12 -in "${KEYSTORE}" -nocerts -out "${KEYSTORE_DIR}/all-services.keystore.key" -passin pass:password -passout pass:password -legacy
 
 log "Setting permissions for keystore files"
 chmod -R 775 "${KEYSTORE_DIR}"
