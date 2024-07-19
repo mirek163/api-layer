a845a160c00fb62b5b9e5b9f0f327d37347a74b0
mirek163 <miroslav.basa.163@gmail.com>
2024-07-19 12:00:08 +0200

    WIP

--- renovate-config.js
+++ renovate-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+    logFileLevel: 'debug',
+    logLevel: 'debug',
     extends: ["config:recommended", "group:allNonMajor"],
     timezone: "Europe/Berlin",
-    schedule: ["after 10am every weekday", "every weekend"],
-    labels: ["renovate"],
+    schedule: ["after 10am every weekday"],
     printConfig: true,
     dependencyDashboard: true,
     baseBranches: ["v3.x.x-renovate", "v2.x.x-renovate"],
