8d59c1e6116936cf737cfa0f425d49e43638fcec
mirek163 <miroslav.basa.163@gmail.com>
2024-07-19 14:41:08 +0200

    Renovate is running

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
      //     matchHost: "api.github.com",
      //     token: process.env.GITHUB_TOKEN
      // }],
-    logFileLevel: 'debug',
-    logLevel: 'debug',
+    // logFileLevel: 'debug',
+    // logLevel: 'debug',
     extends: ["config:recommended", "group:allNonMajor"],
     timezone: "Europe/Berlin",
     schedule: ["after 10am every weekday"],
