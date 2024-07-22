82f151a2464dd13120f9b213f5e4d94d232b4341
mirek163 <miroslav.basa.163@gmail.com>
2024-07-22 18:35:02 +0200

    config

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
     //dryRun: "lookup",
     logFile: "renovate.log",
     repositories: [ { repository: 'mirek163/api-layer' } ],
-    baseBranches: ["v3.x.x-renovate"],
+    baseBranches: ["v3.x.x-renovate", "v2.x.x-renovate"],
     assignees: ["mirek163"],
     prCreation: "not-pending",
     automerge: true,
