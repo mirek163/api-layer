90b54a86411aec984f46c5effce271846309f07f
mirek163 <miroslav.basa.163@gmail.com>
2024-07-22 10:46:08 +0200

    Trying to use created key

--- config.js
+++ config.js
@@ -5,13 +5,14 @@ module.exports = {
      // }],
     // logFileLevel: 'debug',
     // logLevel: 'debug',
+    $schema: "https://docs.renovatebot.com/renovate-schema.json",
     extends: ["config:recommended", "group:allNonMajor"],
     timezone: "Europe/Berlin",
     schedule: ["after 10am every weekday"],
     printConfig: true,
     dependencyDashboard: true,
     baseBranches: ["v3.x.x-renovate", "v2.x.x-renovate"],
-    dryRun: "null",
+    dryRun: "lookup",
     logFile: "renovate.log",
     assignees: ["mirek163"]
 };
