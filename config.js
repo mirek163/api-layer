2df6c3d35a1c7933928b16d7bb3a960f96d0a920
mirek163 <miroslav.basa.163@gmail.com>
2024-07-22 16:04:26 +0200

    WIP

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ module.exports = {
     //    matchHost: "api.github.com",
     //    token: process.env.RENOVATE_TOKEN
     //}],
-    token: process.env.RENOVATE_TOKEN,
     extends: ["config:recommended", "group:allNonMajor"],
     timezone: "Europe/Berlin",
     schedule: ["after 10am every weekday"],
@@ -14,9 +13,9 @@ module.exports = {
     dryRun: "lookup",
     logFile: "renovate.log",
     //autodiscoverFilter: ["mirek163/api-layer"],
-    repositories: [ { repository: 'mirek163/api-layer', bumpVersion: true } ],
+    repositories: [ { repository: 'mirek163/TavernaBot', bumpVersion: true } ],
     //repositories: ["mirek163/api-layer"],
-    baseBranches: ["v3.x.x-renovate"], //"v2.x.x-renovate"
+    //baseBranches: ["v3.x.x-renovate"], //"v2.x.x-renovate"
     //onboarding: "true",
     //forkProcessing: true,
     //enabledManagers: ['github-actions'],
