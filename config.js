41079fd65a389f8c99c07622700a2509534f0713
mirek163 <miroslav.basa.163@gmail.com>
2024-07-22 15:54:08 +0200

    Trying to use created key

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
+
 module.exports = {
     // $schema: "https://docs.renovatebot.com/renovate-schema.json",
+    hostRules:[{
+        matchHost: "api.github.com",
+        token: process.env.RENOVATE_TOKEN
+    }],
     extends: ["config:recommended", "group:allNonMajor"],
     timezone: "Europe/Berlin",
     schedule: ["after 10am every weekday"],
@@ -11,8 +16,8 @@ module.exports = {
     repositories: [ { repository: 'mirek163/api-layer', bumpVersion: true } ],
     //repositories: ["mirek163/api-layer"],
     baseBranches: ["v3.x.x-renovate"], //"v2.x.x-renovate"
+    //onboarding: "true",
     //forkProcessing: true,
-    onboarding: "true",
     //enabledManagers: ['github-actions'],
     //trustLevel: 'high',
     assignees: ["mirek163"]
