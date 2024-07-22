54ec260f2b8b7ee495bf9d823751a38da6d2822a
mirek163 <miroslav.basa.163@gmail.com>
2024-07-22 17:37:39 +0200

    W/o dryrun

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 
 module.exports = {
-    // $schema: "https://docs.renovatebot.com/renovate-schema.json",
-    //hostRules:[{
-    //    matchHost: "api.github.com",
-    //    token: process.env.RENOVATE_TOKEN
-    //}],
-    extends: ["config:recommended"],
 
-    //autodiscoverFilter: ["mirek163/api-layer"],
-    repositories: [ { repository: 'mirek163/api-layer', bumpVersion: true } ],
-    //repositories: ["mirek163/api-layer"],
-    baseBranches: ["v3.x.x-renovate"], //"v2.x.x-renovate"
-    //onboarding: "true",
-    //forkProcessing: true,
-    //enabledManagers: ['github-actions'],
-    //trustLevel: 'high',
+    extends: ["config:recommended", "group:allNonMajor"],
+    timezone: "Europe/Berlin",
+    schedule: ["after 10am every weekday"],
+    printConfig: true,
+    dependencyDashboard: true,
+    //dryRun: "lookup",
+    logFile: "renovate.log",
+    repositories: [ { repository: 'mirek163/api-layer' } ],
+    baseBranches: ["v3.x.x-renovate"],
+    assignees: ["mirek163"]
 };
 
