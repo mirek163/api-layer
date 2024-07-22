4e62bd693532f2c8d0851e3238ffe57c0d7f4a80
mirek163 <miroslav.basa.163@gmail.com>
2024-07-22 13:04:50 +0200

    Trying to use created key

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ module.exports = {
     schedule: ["after 10am every weekday"],
     printConfig: true,
     dependencyDashboard: true,
-    //dryRun: "lookup",
+    dryRun: "lookup",
     logFile: "renovate.log",
-    autodiscoverFilter: ["api-layer"],
-    //repositories: ["mirek163/api-layer"],
+    //autodiscoverFilter: ["mirek163/api-layer"],
+    repositories: ["mirek163/api-layer"],
     baseBranches: ["v3.x.x-renovate"], //"v2.x.x-renovate"
 
     //onboarding: "false",
