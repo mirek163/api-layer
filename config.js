42dbf2085c630f6e4033563b46b426ac43edab4c
mirek163 <miroslav.basa.163@gmail.com>
2024-07-23 12:40:01 +0200

    trying :switchToGradleLite

--- config.js
+++ config.js
@@ -2,20 +2,19 @@
 module.exports = {
     autodiscover: false,
     platform: "github",
-    extends: ["config:recommended", "group:allNonMajor"],
+    extends: ["config:recommended", "group:allNonMajor", ":switchToGradleLite"],
     timezone: "Europe/Berlin",
-    //schedule: ["after 10am every weekday"],
-    //printConfig: true,
     dependencyDashboard: true,
-    //dryRun: "lookup",
     logFile: "renovate.log",
     repositories: ['mirek163/api-layer'],
-    baseBranches: ["v3.x.x-renovate"],
+    baseBranches: ["v2.x.x-renovate"],
     assignees: ["mirek163"],
-    recreateWhen: "always",
-    trustLevel: "high"
-
-  //  prCreation: "not-pending",
-   // retainStalePrs: true
+    //schedule: ["after 10am every weekday"],
+    //printConfig: true,
+    //recreateWhen: "always"
+    //dryRun: "lookup",
+    //trustLevel: "high"
+    //prCreation: "not-pending",
+    // retainStalePrs: true
 };
 
