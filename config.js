3322891be2ed68eae28b57bb82806020edcac988
mirek163 <miroslav.basa.163@gmail.com>
2024-07-23 13:42:17 +0200

    WIP

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ module.exports = {
     repositories: ['mirek163/api-layer'],
     baseBranches: ["v3.x.x-renovate"],
     assignees: ["mirek163"],
-    //schedule: ["after 10am every weekday"],
-    //printConfig: true,
+    schedule: ["after 10am every weekday"],
+    printConfig: true
     //recreateWhen: "always"
     //dryRun: "lookup",
-    trustLevel: "high"
+    //trustLevel: "high"
     //prCreation: "not-pending",
     // retainStalePrs: true
 };
