afde39bb58fa30f4b551575e7ec0927237e9d482
mirek163 <miroslav.basa.163@gmail.com>
2024-07-23 10:02:02 +0200

    WIP

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 
 module.exports = {
-
     extends: ["config:recommended", "group:allNonMajor"],
     timezone: "Europe/Berlin",
     schedule: ["after 10am every weekday"],
     labels: ["renovate"],
-    //printConfig: true,
+    printConfig: true,
     dependencyDashboard: true,
     //dryRun: "lookup",
     logFile: "renovate.log",
     repositories: [ { repository: 'mirek163/api-layer' } ],
     baseBranches: ["v2.x.x-renovate"],
     assignees: ["mirek163"],
+    //recreateWhen: "always",
+
   //  prCreation: "not-pending",
    // retainStalePrs: true
 };
