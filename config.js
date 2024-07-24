0e5afc4e5d2e9b42be43101e2cff48d10f0117a7
mirek163 <miroslav.basa.163@gmail.com>
2024-07-24 15:46:50 +0200

    Trying if it works for v2.x.x-renovate

--- config.js
+++ config.js
@@ -1,40 +1,17 @@
 
 module.exports = {
-    autodiscover: false,
-    platform: "github",
-    //onboardingConfig: { "extends": ["group:allNonMajor", "config:base", ":automergeMinor"] }, //":switchToGradleLite"
     extends: ["group:allNonMajor", "config:recommended"],
-    //extends: ["config:recommended", "group:allNonMajor", ":switchToGradleLite"]
-    //FILTER NODE.js
-    // packageRules: [
-    //     {
-    //         "matchPackagePatterns": ["*"],
-    //         "matchUpdateTypes": ["minor", "patch"],
-    //         "excludePackageNames": ["node"],
-    //         "groupName": "all non-major dependencies except Node.js",
-    //         "groupSlug": "all-minor-patch-except-node"
-    //     }
-    // ],
-//    extends: ["config:recommended", "group:allNonMajor", ":switchToGradleLite"],
     timezone: "Europe/Berlin",
     dependencyDashboard: true,
     logFile: "renovate.log",
     repositories: ['mirek163/api-layer'],
-    baseBranches: ["v3.x.x-renovate"],
+    baseBranches: ["v2.x.x-renovate"],
     assignees: ["mirek163"],
-    schedule: ["after 8am every weekday"],
+    schedule: ["after 6am every weekday"],
     printConfig: true,
     recreateWhen: "always",
     prHourlyLimit: 0,
-    //updateLockFiles: false,
     npmrcMerge: true,
-    npmrc: "legacy-peer-deps=true"
-    //prCreation: "not-pending",
-    //recreateClosed: true
-    //dryRun: "lookup"
-    //trustLevel: "high"
-    //prCreation: "not-pending",
-    // retainStalePrs: true
-    // ":prConcurrentLimitNone"
+    npmrc: "legacy-peer-deps=true" //for updating lock-files
 };
 
