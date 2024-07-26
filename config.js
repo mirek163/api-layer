13f68e38803964a0bf43fe09446ad239f3c527db
mirek163 <95099003+mirek163@users.noreply.github.com>
2024-07-26 10:27:50 +0200

    Update config.js
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     dependencyDashboard: true,
     logFile: "renovate.log",
     repositories: ['mirek163/api-layer'],
-    baseBranches: ["v2.x.x-renovate"],
+    baseBranches: ["v3.x.x-renovate"],
     assignees: ["mirek163"],
     schedule: ["after 6am every weekday"],
     printConfig: true,
