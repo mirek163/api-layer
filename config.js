5490242e634cbd8d71cdd499a20cb48e1b6a3cec
mirek163 <miroslav.basa.163@gmail.com>
2024-07-26 11:26:37 +0200

    wip

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 
 module.exports = {
     extends: ["group:allNonMajor", "config:recommended"],
-    timezone: "Europe/Berlin",
+    timezone: "Europe/Prague",
     dependencyDashboard: true,
     logFile: "renovate.log",
     repositories: ['mirek163/api-layer'],
     baseBranches: ["v3.x.x-renovate"],
     assignees: ["mirek163"],
-    schedule: ["after 6am every weekday"],
     printConfig: true,
     recreateWhen: "always", // recreates all closed or blocking PRs not just immortals
     prHourlyLimit: 0, // removes rate limit for PR creation per hour
