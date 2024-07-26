e5ce4be4153a8d0546577a4641a63a8c2dc602b7
mirek163 <miroslav.basa.163@gmail.com>
2024-07-26 19:15:26 +0200

    wip

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
-
 module.exports = {
     extends: ["group:allNonMajor", "config:recommended"],
-    timezone: "Europe/Prague",
     dependencyDashboard: true,
     logFile: "renovate.log",
     repositories: ['mirek163/api-layer'],
     baseBranches: ["v3.x.x-renovate"],
-    assignees: ["mirek163"],
+    assignees: [],
     printConfig: true,
     recreateWhen: "always", // recreates all closed or blocking PRs not just immortals
     prHourlyLimit: 0, // removes rate limit for PR creation per hour
     npmrc: "legacy-peer-deps=true", //for updating lock-files
     npmrcMerge: true //be combined with a "global" npmrc
-
 };
 
