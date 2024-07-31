90d39db409b4fb71dd2bc25fca5def353f977b33
mirek163 <miroslav.basa.163@gmail.com>
2024-07-31 13:26:12 +0200

    adding label and prTitle

--- renovate_config.js
+++ renovate_config.js
@@ -4,6 +4,8 @@ module.exports = {
     repositories: ['mirek163/api-layer'],
     baseBranches: ["v3.x.x-renovate"],
     assignees: [],
+    labels: ["dependencies"],
+    prTitle: "renovate: Update dependency {{depName}} to v{{newVersion}}",
     printConfig: true,
     recreateWhen: "always", // recreates all closed or blocking PRs not just immortals
     prHourlyLimit: 0, // removes rate limit for PR creation per hour
