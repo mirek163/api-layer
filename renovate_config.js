414cadb4fbeea1056535f8ab489cc6e8e21a669d
mirek163 <miroslav.basa.163@gmail.com>
2024-07-31 16:54:58 +0200

    WIP

--- renovate_config.js
+++ renovate_config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    extends: ["group:allNonMajor", "config:recommended"],
+    extends: ["group:allNonMajor"],
     dependencyDashboard: true,
     repositories: ['mirek163/api-layer'],
     baseBranches: ["v3.x.x-renovate"],
     assignees: [],
     labels: ["dependencies"],
     commitMessagePrefix:"chore: " ,
-    //prTitle: "chore: Update dependency {{depName}} to v{{newVersion}}",
     printConfig: true,
     prHourlyLimit: 0, // removes rate limit for PR creation per hour
     npmrc: "legacy-peer-deps=true", //for updating lock-files
