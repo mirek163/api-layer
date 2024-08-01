62942e35947ba222312125d242ca0d94fb6cb5fa
mirek163 <miroslav.basa.163@gmail.com>
2024-08-01 11:57:18 +0200

    WIP

--- renovate_config.js
+++ renovate_config.js
@@ -1,16 +1,38 @@
 module.exports = {
-
-    extends: ["config:recommended", "group:allNonMajor"],
-    //ignorePresets: ["group:monorepos", "group:recommended"],
+    extends: ["config:recommended"],
+    packageRules: [
+        {
+            groupName: "all non-major dependencies",
+            groupSlug: "all-minor-patch",
+            matchPackageNames: ["*"],
+            matchUpdateTypes: ["minor", "patch"]
+        },
+        {
+            groupName: "eslint",
+            matchPackagePatterns: ["^eslint", "^@eslint"]
+        }
+    ],
+//extends: ["config:recommended", "group:allNonMajor"],
+//ignorePresets: ["group:monorepos", "group:recommended"],
     dependencyDashboard: true,
-    repositories: ['mirek163/api-layer'],
-    baseBranches: ["v3.x.x-renovate"],
-    assignees: [],
-    labels: ["dependencies"],
-    commitMessagePrefix:"chore: " ,
-    printConfig: true,
-    prHourlyLimit: 0, // removes rate limit for PR creation per hour
-    npmrc: "legacy-peer-deps=true", //for updating lock-files
-    npmrcMerge: true //be combined with a "global" npmrc
-};
+    repositories:
+        ['mirek163/api-layer'],
+    baseBranches:
+        ["v3.x.x-renovate"],
+    assignees:
+        [],
+    labels:
+        ["dependencies"],
+    commitMessagePrefix:
+        "chore: ",
+    printConfig:
+        true,
+    prHourlyLimit:
+        0, // removes rate limit for PR creation per hour
+    npmrc:
+        "legacy-peer-deps=true", //for updating lock-files
+    npmrcMerge:
+        true //be combined with a "global" npmrc
+}
+;
 
