4c9a1b5babe3003102feaec3a1f6e25c4f494a9c
mirek163 <miroslav.basa.163@gmail.com>
2024-08-27 15:02:05 +0200

    excluding grouping monorepos

--- renovate_config.js
+++ renovate_config.js
@@ -1,15 +1,17 @@
 module.exports = {
-    extends: [":dependencyDashboard", ":semanticPrefixFixDepsChoreOthers", ":ignoreModulesAndTests","group:recommended", "replacements:all", "workarounds:all"],
-    //extends: ["config:recommended"],
+    extends: ["config:recommended"],
     packageRules: [
         {
+            //for v.3.x.x branch find all Packages which are patch, slug them and make PR with name "all patch dependencies"
             "matchBaseBranches": ["v3.x.x-renovate"],
+            "ignorePresets": ["group:monorepos", "group:recommended"],
             "groupName": "all patch dependencies",
             "groupSlug": "all-patch",
             "matchPackageNames": ["*"],
             "matchUpdateTypes": ["patch"],
         },
         {
+            //for v.3.x.x make dashboard approval to all major and minor dependencies updates
             "matchBaseBranches": ["v3.x.x-renovate"],
             "matchUpdateTypes": ["major", "minor"],
             "dependencyDashboardApproval": true,
