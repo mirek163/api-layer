f9f9df653df5a8a7b0d82070baa1f290ae4c2337
mirek163 <miroslav.basa.163@gmail.com>
2024-08-27 16:30:12 +0200

    Working solution

--- renovate_config.js
+++ renovate_config.js
@@ -1,37 +1,40 @@
 module.exports = {
-    extends: ["config:recommended"],
+    globalExtends: ["config:recommended"], // instead of "extends" this resolve immediately as part of global config
+    repositories: ['mirek163/api-layer'],
+    baseBranches: ['v2.x.x','v3.x.x-renovate'],
+    dependencyDashboard: true,
     packageRules: [
         {
-            //for v.3.x.x branch find all Packages which are patch, slug them and make PR with name "all patch dependencies"
-            "matchBaseBranches": ["v3.x.x-renovate"],
-            "ignorePresets": ["group:monorepos", "group:recommended"],
+            //for v.2.x.x branch ignore grouping from extends preset, find all packages which are patches,
+            // slug them and make PR with name "all patch dependencies"
+            "matchBaseBranches": ["v2.x.x"],
             "groupName": "all patch dependencies",
             "groupSlug": "all-patch",
             "matchPackageNames": ["*"],
             "matchUpdateTypes": ["patch"],
         },
         {
-            //for v.3.x.x make dashboard approval to all major and minor dependencies updates
-            "matchBaseBranches": ["v3.x.x-renovate"],
+            //for v.2.x.x make dashboard approval to all major and minor dependencies updates
+            "matchBaseBranches": ["v2.x.x"],
             "matchUpdateTypes": ["major", "minor"],
             "dependencyDashboardApproval": true,
         },
-        // {
-        //     "matchBaseBranches": ["v2.x.x"],
-        //     "groupName": "all non-major dependencies",
-        //     "groupSlug": "all-minor-patch",
-        //     "matchPackageNames": ["*"],
-        //     "matchUpdateTypes": ["minor", "patch"]
-        // },
-        // {
-        //     "matchBaseBranches": ["v2.x.x"],
-        //     "matchUpdateTypes": ["major"],
-        //     "dependencyDashboardApproval": true,
-        // }
+        {
+            //for v.3.x.x branch find all packages which are minor and patches,
+            // slug them and make PR with name "all non-major dependencies"
+             "matchBaseBranches": ["v3.x.x-renovate"],
+             "groupName": "all non-major dependencies",
+             "groupSlug": "all-minor-patch",
+             "matchPackageNames": ["*"],
+             "matchUpdateTypes": ["minor", "patch"]
+         },
+         {
+             //for v.3.x.x make dashboard approval to all major dependencies updates
+             "matchBaseBranches": ["v3.x.x-renovate"],
+             "matchUpdateTypes": ["major"],
+             "dependencyDashboardApproval": true,
+         }
     ],
-    dependencyDashboard: true,
-    repositories: ['mirek163/api-layer'],
-    baseBranches: ['v3.x.x-renovate'],
     printConfig: true,
     labels: ['dependencies'],
     dependencyDashboardLabels: ['dependencies'],
