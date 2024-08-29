5f44f9e7d450e0fef376553589b2b0f1f6293f9e
mirek163 <95099003+mirek163@users.noreply.github.com>
2024-08-29 21:21:15 +0200

    Update renovate_config.js
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>

--- renovate_config.js
+++ renovate_config.js
@@ -22,10 +22,8 @@ module.exports = {
         {
             {
            "matchDatasources": ["npm"],
-           "registryUrls": [
-            "https://zowe.jfrog.io/artifactory/api/npm/npm-org/"
-            ]
-        }
+           "registryUrls": ["https://zowe.jfrog.io/artifactory/api/npm/npm-org/"]
+            }
             //for v.2.x.x branch ignore grouping from extends preset, find all packages which are patches,
             // slug them and make PR with name "all patch dependencies"
             "matchBaseBranches": ["v2.x.x"],
